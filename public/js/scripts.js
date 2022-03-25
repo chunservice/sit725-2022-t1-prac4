1a809b272b6258c0179fe70409ada46ae1974f5a
chunservice <anson.it@yahoo.com>
2022-03-25 19:35:37 +1100

    Updates: better design

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -43,3 +43,15 @@ $(document).ready(function(){
         })
         addCards(cardList);
     });
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.modal');
+    var instances = M.Modal.init(elems, options);
+    });
+
+    // Or with jQuery
+
+    $(document).ready(function(){
+    $('.modal').modal();
+    });

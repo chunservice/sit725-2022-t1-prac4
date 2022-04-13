a513004da3a7180c432da436d6102d502f94af20
chunservice <anson.it@yahoo.com>
2022-04-13 20:37:21 +1000

    Updates: move cardList from frontend to backend

--- server.js
+++ server.js
@@ -6,6 +6,27 @@ app.use(express.static(__dirname+'/public'))
 app.use(express.json());
 app.use(express.urlencoded({ extended: false}));
 
+const cardList = [    
+    {
+        title: "Orange",
+        image: "images/orange_cut.png",
+        link: "About Orange",
+        description: "An orange is a fruit of various citrus species in the family Rutaceae."
+     },
+    {
+       title: "Lime",
+       image: "images/lime_cut.png",
+       link: "About Lime",
+       description: "A lime is a citrus fruit, which is typically round, green in color and contains acidic juice vesicles."
+    },
+    {
+        title: "Lemon",
+        image: "images/lemon_cut.png",
+        link: "About Lemon",
+        description: "A lemon is a species of small evergreen trees in the flowering plant family Rutaceae."
+    }
+]
+
 // define the port as 3000
 var port = process.env.port || 3000;
 
